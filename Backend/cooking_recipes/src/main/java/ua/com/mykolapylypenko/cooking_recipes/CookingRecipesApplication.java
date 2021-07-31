b585db99bcb6d9cbb3889ea951ad9ae0f2dfc323
Mykola Pylypenko <mr.gigami@gmail.com>
2021-07-31 12:08:29 +0300

    initialize project

---
+++ Backend/cooking_recipes/src/main/java/ua/com/mykolapylypenko/cooking_recipes/CookingRecipesApplication.java
@@ -0,0 +1,13 @@
+package ua.com.mykolapylypenko.cooking_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingRecipesApplication.class, args);
+	}
+
+}
