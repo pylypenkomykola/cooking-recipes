860fb683d9a4e6ff2fe09fba270367cbcb0a569e
Mykola Pylypenko <mr.gigami@gmail.com>
2021-08-09 21:03:04 +0300

    create: spring security superclass

---
+++ Backend/cooking_recipes/src/main/java/ua/com/mykolapylypenko/cooking_recipes/security/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package ua.com.mykolapylypenko.cooking_recipes.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception{
+
+    }
+}
